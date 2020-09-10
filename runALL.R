3695b15ee107ca17ec885c4bea399c5cd223ac32
APsofiaXYZ <61021481+APsofiaXYZ@users.noreply.github.com>
2020-09-10 22:53:35 +0200

    Update runALL.R

--- runALL.R
+++ runALL.R
@@ -6,32 +6,29 @@
 # Small sample data could be found in the related repository.
 # Please install Rcpp and RcppArmadillo packages. AP.
 # The corresponding Cpp file with DMH algorithm and the functions attached.
-# The cpp code is currently not fully debugged, hence this code stops at some point during the full cycle of iterations. 
+# The cpp code is currently not fully debugged, it stop at some point during the full cycle of iterations. 
 
 ############
 
-library(coda)  
 library(RcppArmadillo) 
 library(Rcpp)
 
-
 Rcpp::sourceCpp('DMH.cpp')
 
-s2<- read.table('11299_6_Info.txt', header = T)
+s<- read.table('11446_6_Info-201p.txt', header = T)
+X<-s$x
+Y<-s$y
+Z<-as.integer(s$z)
 
-X<-s2$X
-Y<-s2$Y
-Z<-as.numeric(s2$Z)
 Qnumber<-3
-
-C<-0.05
+C<-0.1
 N<-length(X)
 
-Tau<-0.1 # sqrt
-M<-2*length(X) # should be multiple of length
+Tau<-sqrt(0.1) # sqrt
+M<-1*length(X) # should be multiple of length
 
 # iterations for DMC
-Iter <- 20000
+Iter <- 50000
 
 # define set of possible types for cells
 Marks<-1:Qnumber
@@ -40,19 +37,17 @@ Marks<-1:Qnumber
 ### PRIOR DISTRIBUTIONS ###
 # set the hyperparameters 
 
-Mu_omega <- 0 # as suggested by Li
+Mu_omega <- 1 # as suggested by Li
 Sigma_omega <- 1 # as suggested by Li
 Mu_theta <- 0 # as suggested by Li
 Sigma_theta <- 1 # as suggested by Li
-
 A_lambda <- 0.001 # as suggested by Li, Gelman
 B_lambda <- 0.001
 Lambda<-rgamma(1, 1, 1)
 
-
 # ==== Initialization for chain ====
-# omega vector
 Omega_start <-rep(1,Qnumber)
+Omega_start [1]<-rnorm(1, Mu_omega, 1)
 
 # Theta matrix, symmetric, let's generate from priors 
 Theta_start <- matrix(0,Qnumber,Qnumber)
@@ -80,12 +75,8 @@ for (l in 1:N) {
 
 
 # ======== DMH
-# DMH(NumericVector omega, NumericVector omega_start, NumericMatrix theta,
-#    NumericVector x, NumericVector y, NumericVector z, arma::vec  marks,
-#    NumericMatrix dmatrix, int iter, int M, int n, int Q, double c,  
-#    double lambda, double mu_omega, double sigma_omega, double tau2, ...)  
 
-final<-DMH(Omega, Omega_start, Theta, X, Y, Z, Marks, Dmatrix, Iter, M, N, Qnumber, C, Lambda, Mu_omega, Sigma_omega, Tau, Mu_theta, Sigma_theta, A_lambda, B_lambda)
+final<-DMH(X, Y, Z, Marks, Dmatrix, Iter, M, N, Qnumber, C, Lambda, Mu_omega, Sigma_omega, Tau, Mu_theta, Sigma_theta, A_lambda, B_lambda, Omega_start, Theta_start)
 
 omega_matrix<-final$omega_matrix
 acceptomega<-final$accept_omega
@@ -93,13 +84,8 @@ accepttheta<-final$accept_theta
 theta_matrix<-final$theta_matrix
 la_vec<-final$lambda_vec
 accept_la<-final$accept_la
-zstar_matrix<-final$zstar_mat
 
 
 save(omega_matrix,acceptomega, accepttheta, theta_matrix, 
-     la_vec, accept_la, zstar_matrix, Tau, Iter, M, N, 
-     file = "11299_6_Info_result_20th_M2_Tau01.RData")
-
-
-
-
+     la_vec, accept_la, Tau, Iter, M, N, 
+     file = "pat_p100012_201p.RData")
